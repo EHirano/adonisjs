9ac7d87328b8659c01d523a2dccbbe7ce9df72a5
EHirano <eduardoloverahirano@gmail.com>
2022-06-20 20:58:17 -0300

    controller, model e migration da tabela Users

--- japaFile.ts
+++ japaFile.ts
@@ -2,12 +2,18 @@ import getPort from 'get-port'
 import { configure } from 'japa'
 import { join } from 'path'
 import sourceMapSupport from 'source-map-support'
-
+import execa from 'execa'
 import 'reflect-metadata'
 
-process.env.NODE_ENV = 'testing'
+process.env.NODE_ENV = 'test'
 process.env.ADONIS_ACE_CWD = join(__dirname)
 sourceMapSupport.install({ handleUncaughtExceptions: false })
+
+async function runMigrations() {
+  await execa.node('ace', ['migration:run'], {
+    stdio: 'inherit',
+  })
+}
 
 async function startHttpServer() {
   const { Ignitor } = await import('@adonisjs/core/build/src/Ignitor')
