a2ecd9598bd18a4299de9d651c3512728e3345a6
EHirano <eduardoloverahirano@gmail.com>
2022-06-28 19:24:14 -0300

    Add sign out logic and test

--- test/users/sessions.spec.ts
+++ test/users/sessions.spec.ts
@@ -38,7 +38,7 @@ test.group('Session', (group) => {
     assert.equal(body.status, 400)
   })
 
-  test.only('it should return 400 when credentials are invalid are invalid', async (assert) => {
+  test('it should return 400 when credentials are invalid are invalid', async (assert) => {
     const { email } = await UserFactory.create()
     const { body } = await supertest(BASE_URL)
       .post('/sessions')
@@ -48,6 +48,27 @@ test.group('Session', (group) => {
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 400)
     assert.equal(body.message, 'invlaid credentials')
+  })
+
+  test.only('it should return 200 when user signs out', async (assert) => {
+    const plainPassword = 'test'
+    const { email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    const apiToken = body.token
+
+    await supertest(BASE_URL)
+      .delete('/sessions')
+      .set('Authorization', `Bearer ${apiToken.token}`)
+      .expect(200)
+
+    //access the database through lucid to check if the token doesn't exists anymore
+    const token = await Database.query().select('*').from('api_tokens')
+
+    assert.isEmpty(token)
   })
 
   group.beforeEach(async () => {
