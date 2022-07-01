2de288dffa09d88660457f26a87de17972098d68
EHirano <eduardoloverahirano@gmail.com>
2022-07-01 15:27:47 -0300

    Implemented group creation controller and validator

--- test/groups/group.spec.ts
+++ test/groups/group.spec.ts
@@ -7,7 +7,7 @@ import { UserFactory } from './../../database/factories/index'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Group', (group) => {
-  test.only('it should create a group', async (assert) => {
+  test('it should create a group', async (assert) => {
     const user = await UserFactory.create()
     const groupPayload = {
       name: 'test',
@@ -27,6 +27,13 @@ test.group('Group', (group) => {
     assert.equal(body.group.location, groupPayload.location)
     assert.equal(body.group.chronic, groupPayload.chronic)
     assert.equal(body.group.master, groupPayload.master)
+  })
+
+  test.only('it should return 422 when required data is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/groups').send({}).expect(422)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
   })
 
   group.beforeEach(async () => {
